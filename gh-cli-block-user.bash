7870a904b59735e14b0f7c4cf4495bcdbe498e6e
LiamKearn <contact@liamkearn.me>
2026-09-16 17:53:12 +1000

    NO SUBSHELLS!

--- gh-cli-block-user.bash
+++ gh-cli-block-user.bash
@@ -1,20 +1,32 @@
 #!/usr/bin/env bash
 set -ux # Intentionally leave failures, sometimes the user may already have one of these blocked.
 
-# This script blocks known agent users.
+# This script blocks known agent users using the GH CLI
 
 GH_CLI="gh"
 
-if ! hash "$GH_CLI"
+if ! hash "${GH_CLI}"
 then
-    printf "%s is not installed\n" "$GH_CLI"
+    printf "%s is not installed\n" "${GH_CLI}"
     exit 1
 fi
 
-if ! "$GH_CLI" auth status --json hosts --jq '.hosts["github.com"][].scopes' | grep -qE "^user,| user"
+coproc AUTH_STATUS_PROC { "${GH_CLI}" auth status --json hosts --jq '.hosts["github.com"][].scopes'; }
+AUTH_PID="${AUTH_STATUS_PROC_PID:?}"
+
+grep -qE "^user,| user" <&"${AUTH_STATUS_PROC[0]}"
+FOUND_SCOPE=$?
+
+if ! wait "${AUTH_PID}";
+then
+    printf "Could not obtain GH_CLI auth status\n"
+    exit 1
+fi
+
+if [[ "${FOUND_SCOPE}" -ne 0 ]];
 then
     printf "You must have access to the user scope with your github CLI.\n"
-    printf "Try running '%s auth refresh -h github.com -s user'\n" "$GH_CLI"
+    printf "Try running '%s auth refresh -h github.com -s user'\n" "${GH_CLI}"
     exit 1
 fi
 
--- tooling/generate-gh-cli-block-user.sh
+++ tooling/generate-gh-cli-block-user.sh
@@ -15,25 +15,37 @@ set -ux # Intentionally leave failures, sometimes the user may already have one 
 
 GH_CLI="gh"
 
-if ! hash "$GH_CLI"
+if ! hash "${GH_CLI}"
 then
-    printf "%s is not installed\n" "$GH_CLI"
+    printf "%s is not installed\n" "${GH_CLI}"
     exit 1
 fi
 
-if ! "$GH_CLI" auth status --json hosts --jq '.hosts["github.com"][].scopes' | grep -qE "^user,| user"
+coproc AUTH_STATUS_PROC { "${GH_CLI}" auth status --json hosts --jq '.hosts["github.com"][].scopes'; }
+AUTH_PID="${AUTH_STATUS_PROC_PID:?}"
+
+grep -qE "^user,| user" <&"${AUTH_STATUS_PROC[0]}"
+FOUND_SCOPE=$?
+
+if ! wait "${AUTH_PID}";
+then
+    printf "Could not obtain GH_CLI auth status\n"
+    exit 1
+fi
+
+if [[ "${FOUND_SCOPE}" -ne 0 ]];
 then
     printf "You must have access to the user scope with your github CLI.\n"
-    printf "Try running '%s auth refresh -h github.com -s user'\n" "$GH_CLI"
+    printf "Try running '%s auth refresh -h github.com -s user'\n" "${GH_CLI}"
     exit 1
 fi
 
 # BEGIN GENERATED
 BASH
     while read -r LINE; do
-        [[ "$LINE" = "#"* ]] && continue
-        printf "GH_PAGER=\"\" gh api --method PUT /user/blocks/%s\n" "$LINE"
-    done < "$USER_LIST"
+        [[ "${LINE}" = "#"* ]] && continue
+        printf "GH_PAGER=\"\" gh api --method PUT /user/blocks/%s\n" "${LINE}"
+    done < "${USER_LIST}"
     printf "# END GENERATED\n"
-} > "$OUTPUT_FILE_NAME"
+} > "${OUTPUT_FILE_NAME}"
 
